86915f8014e9cea7a12bfb0638ae2502f934beb1
Marjan Haghighat <MarjanHD1372@gmail.com>
2026-04-20 13:26:34 +0100

    Add MongoDB integration and user registration API

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
-const express = require("express");
+﻿const express = require("express");
 const session = require("express-session");
-const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+const axios = require("axios");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+//connect to mongoDb
+mongoose
+  .connect(
+    "mongodb+srv://marjanhaghighat027_db_user:HealAppointment@cluster0.yajswxy.mongodb.net/",
+  )
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.log(err));
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(
   session({
@@ -11,77 +22,71 @@ app.use(
     saveUninitialized: false,
   }),
 );
-const PORT = process.env.PORT || 3000;
 
-// Serve static files from the Public directory
 app.use(express.static(path.join(__dirname, "Public")));
 
-// Database
-const db = new sqlite3.Database("./database.db");
-
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      email TEXT UNIQUE,
-      password TEXT
-    )
-  `);
-});
-// Register API
-app.post("/api/register", (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: "Email and password required" });
+async function sendWebhook(email) {
+  try {
+    await axios.post("https://marjanhd.app.n8n.cloud/webhook/test-user", {
+      name: email,
+      email,
+    });
+    console.log("Webhook sent to n8n");
+  } catch (error) {
+    console.log("Error sending to n8n:", error.message);
   }
+}
 
-  db.run(
-    "INSERT INTO users (email, password) VALUES (?, ?)",
-    [email, password],
-    function (err) {
-      if (err) {
+async function startServer() {
+
+
+  app.post("/api/register", async (req, res) => {
+    const { firstName, lastName, email, password, nhsNumber } = req.body;
+    console.log("DATA RECEIVED:", req.body);
+
+    try {
+      const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+        nhsNumber,
+      });
+      console.log("USER SAVED:", user); // 👈 تست
+      await sendWebhook(email);
+
+      res.json({ message: "User registered", user });
+    } catch (err) {
+      console.log(err);
+
+      if (err.code === 11000) {
         return res.status(400).json({ message: "User already exists" });
       }
 
-      res.json({ message: "User registered successfully" });
-    },
-  );
-});
-// Login API
-app.post("/api/login", (req, res) => {
-  const { email, password } = req.body;
+      res.status(500).json({ message: "Error creating user" });
+    }
+  });
 
-  db.get(
-    "SELECT * FROM users WHERE email = ? AND password = ?",
-    [email, password],
-    (err, user) => {
-      if (!user) {
-        return res.status(401).json({ message: "Invalid credentials" });
-      }
+  app.post("/api/login", async (req, res) => {});
 
-      req.session.user = user;
+  app.get("/api/me", (req, res) => {
+    if (!req.session.user) {
+      return res.status(401).json({ message: "Not logged in" });
+    }
 
-      res.json({ message: "Logged in successfully", user });
-    },
-  );
-});
+    res.json(req.session.user);
+  });
 
-// check user login
-app.get("/api/me", (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).json({ message: "Not logged in" });
-  }
+  app.post("/api/logout", (req, res) => {
+    req.session.destroy();
+    res.json({ message: "Logged out" });
+  });
 
-  res.json(req.session.user);
-});
-app.post("/api/logout", (req, res) => {
-  req.session.destroy();
-  res.json({ message: "Logged out" });
-});
-// Start the server
-app.listen(PORT, () => {
-  console.log(
-    `NHS Booking System server is running on http://localhost:${PORT}`,
-  );
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+startServer().catch((error) => {
+  console.error("Server failed to start:", error.message);
+  process.exit(1);
 });
